d9f482b8fdf587e81e19ef2721b28c9cb8a1ef8f
RickyMetral <ricardometral2005@gmail.com>
2025-07-09 13:53:04 +0000

    Added Quaternion Rotation and RCLCPP logs

--- include/optitrack/VrpnClient.hpp
+++ include/optitrack/VrpnClient.hpp
@@ -16,14 +16,17 @@ private:
 	rclcpp::Node::SharedPtr node;
 	VehicleOdometry odom_msg;
 	vrpn_Connection* connection;//The connection object for vrpn server
-	std::shared_ptr<vrpn_Tracker_Remote> tracker;//The tracker vrpn object
-	//vrpn_Tracker_Remote tracker;//The tracker vrpn object
+	std::shared_ptr<vrpn_Tracker_Remote> tracker;//The vrpn tracker object
+
 private:
-	void setMsg(VehicleOdometry& msg);//Initializes default values for the odometry to be sent on callback
+	void set_msg(VehicleOdometry& msg);//Initializes default values for the odometry to be sent on callback
 	static void VRPN_CALLBACK handle_pose(void* userData, const vrpn_TRACKERCB t);//When Pose data is received, it will publish to the vehcile_mocap_odometry topic
 	static void VRPN_CALLBACK handle_vel(void* userData, const vrpn_TRACKERVELCB t);//When Pose data is received, it will publish to the vehcile_mocap_odometry topic
 
 public:
+	void rotate_quat_arr(double angle, char axis, double (&quat_arr)[4]);//Rotates quaternion array, by given amt of degrees along specified axis. Expexts quat_arr to be (X,Y,Z,W)
+	void log_pose_tracker(const rclcpp::Logger& logger, const vrpn_TRACKERCB& t, const double (&quat)[4]);//Logger for the pose stream
+	void log_vel_tracker(const rclcpp::Logger& logger, const vrpn_TRACKERVELCB& t);//Logger for the velocity stream
 	VrpnClient(std::string rigidBodyName, std::string server_address); //Establishes Connection and registers callbacks
 	~VrpnClient();//Unregisters the tracker callbacks
 	void mainloop();//One iteration of connection and tracker mainloop and sleeps for 10ms
--- src/VrpnClient.cpp
+++ src/VrpnClient.cpp
@@ -7,55 +7,111 @@
 #include "vrpn_Tracker.h"
 #include "vrpn_Connection.h"
 #include "rclcpp/rclcpp.hpp"
-#include "px4_msgs/msg/vehicle_odometry.hpp"
 #include "VrpnClient.hpp"
+#include "px4_msgs/msg/vehicle_odometry.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 using namespace px4_msgs::msg;
 
 
-std::ostream& operator<<(std::ostream& os, const vrpn_TRACKERCB t)
+void VrpnClient::log_pose_tracker(const rclcpp::Logger& logger, const vrpn_TRACKERCB& t, const double (&quat)[4])
 {
-   os << "Quaternions: " << t.quat[3] << " " << t.quat[0]
-   << " " <<  t.quat[1] << " " << t.quat[2] << "\n"
-   "Position: " << t.pos[0] << " "  <<  t.pos[1] << " " << t.pos[2] << std::endl;
-   return os;
+   RCLCPP_INFO(logger,
+   "Quaternions: %.3f %.3f %.3f %.3f\nPosition: %.3f %.3f %.3f",
+   quat[3], quat[0], quat[1], quat[2],
+   t.pos[0], t.pos[1], t.pos[2]
+   );
 }
 
-std::ostream& operator<<(std::ostream& os, const vrpn_TRACKERVELCB t)
+void VrpnClient::log_vel_tracker(const rclcpp::Logger& logger, const vrpn_TRACKERVELCB& t)
 {
-    os << "Velocity Quat: " << t.vel_quat[3] << " " << t.vel_quat[0] 
-        <<  t.vel_quat[1] << " " << t.vel_quat[2] << "\n"
-        "Velocity: " << t.vel[0] << " " <<  t.vel[1] << " " << t.vel[2]  << std::endl;
-    return os;
+   RCLCPP_INFO(logger,
+   "Velocity Quaternions: %.3f %.3f %.3f %.3f\nVelocity: %.3f %.3f %.3f",
+   t.vel_quat[3], t.vel_quat[0], t.vel_quat[1], t.vel_quat[2],
+   t.vel[0], t.vel[1], t.vel[2]
+   );
 }
 
-void VrpnClient::setMsg(VehicleOdometry& msg)
+
+void VrpnClient::rotate_quat_arr(double angle, char axis, double(&quat_arr)[4])
 {
+   //Transform our array into tf2 
+   tf2::Quaternion q(quat_arr[0], quat_arr[1], quat_arr[2], quat_arr[3]);//(X, Y, Z, W)
+   
+   constexpr double DEG2RAD = M_PI/ 180.0;//Conversion constant
+   double rotate_rad = angle * DEG2RAD;//Converts angle to radians 
+
+   tf2::Quaternion q_transform; 
+
+   //Decdiing which axis to rotate around
+   if(axis == 'X' ||  axis == 'x')
+   {
+   	q_transform.setRPY(rotate_rad, 0, 0);
+   }
+   else if(axis == 'Y' ||  axis == 'y')
+   {
+   	q_transform.setRPY(0, rotate_rad, 0);
+   }
+   else if(axis == 'Z' ||  axis == 'z')
+   {
+   	q_transform.setRPY(0, 0, rotate_rad);
+   } 
+   else
+   {
+	std::cerr << "Invalid Rotation Axis Provided" << std::endl;
+   }
+
+   tf2::Quaternion res = q_transform * q;//Resultant vector
+   res.normalize();
+   
+   quat_arr[0] = static_cast<double>(res.x());
+   quat_arr[1] = static_cast<double>(res.y());
+   quat_arr[2] = static_cast<double>(res.z());
+   quat_arr[3] = static_cast<double>(res.w());
+} 
+
+void VrpnClient::set_msg(VehicleOdometry& msg)
+{
+
    //Defines our coordinate frames
    msg.pose_frame = VehicleOdometry::POSE_FRAME_NED;
    msg.velocity_frame = VehicleOdometry::VELOCITY_FRAME_NED;
    
    msg.reset_counter = 0;
    msg.quality = 100;
+   
    //These are optional covariance matrices
-   msg.position_variance[0] = NAN;
-   msg.orientation_variance[0] = NAN;
+   msg.position_variance = {0.0001, 0.0001, 0.001};
+   msg.orientation_variance = {0.0001, 0.0001, 0.001};
    msg.velocity_variance[0] = NAN;
+
 }
 
 //Callback function for receiving tracker velocity data
 void VRPN_CALLBACK VrpnClient::handle_vel(void* userData, const vrpn_TRACKERVELCB t)
 {
-  VrpnClient* self = static_cast<VrpnClient*> (userData);
+   VrpnClient* self = static_cast<VrpnClient*> (userData);
 
-  self->odom_msg.velocity[0] = t.vel[0];//(x,y,z), motive has Y as vertical axis
-  self->odom_msg.velocity[1] = t.vel[2];
-  self->odom_msg.velocity[2] = -t.vel[1];
+   double quat_arr[4] = {t.vel_quat[0], t.vel_quat[1], t.vel_quat[2], t.vel_quat[3]};
 
-  self->odom_msg.angular_velocity[0] = t.vel_quat[3];//(w,x,y,z)
-  self->odom_msg.angular_velocity[1] = t.vel_quat[0];
-  self->odom_msg.angular_velocity[2] = t.vel_quat[1];
-  self->odom_msg.angular_velocity[3] = t.vel_quat[2];
+   rclcpp::Clock steady_clock(RCL_STEADY_TIME);//Time since boot
+   self->odom_msg.timestamp = steady_clock.now().nanoseconds() / 1000;//timestamp is the current time since boot
+   self->odom_msg.timestamp_sample = self->odom_msg.timestamp - 10;//Timestamp_sample is when sample was taken from mocap
+
+
+   self->odom_msg.velocity[0] = t.vel[0];//(x,y,z), motive has Y as vertical axis
+   self->odom_msg.velocity[1] = t.vel[2];
+   self->odom_msg.velocity[2] = -t.vel[1];
+
+   self->rotate_quat_arr(90, 'x', quat_arr);
+
+   self->odom_msg.angular_velocity[0] = quat_arr[3];//(w,x,y,z)
+   self->odom_msg.angular_velocity[1] = quat_arr[0];
+   self->odom_msg.angular_velocity[2] = quat_arr[1];
+   self->odom_msg.angular_velocity[3] = quat_arr[2];
+
+   self->log_vel_tracker(self->node->get_logger(), t);
 }
 
 //Callback function for receiving tracker position data
@@ -63,16 +119,29 @@ void VRPN_CALLBACK VrpnClient::handle_pose(void* userData, const vrpn_TRACKERCB 
 {
     VrpnClient* self = static_cast<VrpnClient*> (userData);
 
+    double quat_arr[4] = {t.quat[0], t.quat[1], t.quat[2], t.quat[3]};
+
+    rclcpp::Clock steady_clock(RCL_STEADY_TIME);//Time since boot
+    self->odom_msg.timestamp = steady_clock.now().nanoseconds() / 1000;//timestamp is the current time since boot
+    self->odom_msg.timestamp_sample = self->odom_msg.timestamp - 10;//Timestamp_sample is when sample was taken from mocap
+
+
     self->odom_msg.position[0] = t.pos[0];//(x,y,z), motive has Y as vertical axis
     self->odom_msg.position[1] = t.pos[2];
     self->odom_msg.position[2] = -t.pos[1];
 
-    self->odom_msg.q[0] = t.quat[3];//(w,x,y,z)
-    self->odom_msg.q[1] = t.quat[0];
-    self->odom_msg.q[2] = t.quat[1];
-    self->odom_msg.q[3] = t.quat[2];
+    self->rotate_quat_arr(90, 'x', quat_arr);
+
+    self->odom_msg.q[0] = quat_arr[3];//(w,x,y,z)
+    self->odom_msg.q[1] = quat_arr[0];
+    self->odom_msg.q[2] = quat_arr[1];
+    self->odom_msg.q[3] = quat_arr[2];
+
+    self->odom_msg.reset_counter = 0;
+    self->odom_msg.quality = 90;
+
     self->odom_publisher_->publish(self->odom_msg);
-    std::cout << t << std::endl;
+    self->log_pose_tracker(self->node->get_logger(), t, quat_arr);
 }
 
 
@@ -81,7 +150,7 @@ VrpnClient::VrpnClient(std::string rigidBodyName, std::string server_address)
 {
     // Set your tracker name & VRPN server address
     const std::string full_address = rigidBodyName + "@" + server_address;
-    setMsg(odom_msg);
+    set_msg(odom_msg);//Sets the default parameters for our message
     this->node = rclcpp::Node::make_shared("publish_motive_odometry_node");
     this->odom_publisher_ = node->create_publisher<VehicleOdometry>("/fmu/in/vehicle_mocap_odometry", 10); 
 
@@ -99,7 +168,7 @@ VrpnClient::VrpnClient(std::string rigidBodyName, std::string server_address)
 
     // Register callback function
     tracker->register_change_handler(this, &VrpnClient::handle_pose);
-    tracker->register_change_handler(this, &VrpnClient::handle_vel);
+    //tracker->register_change_handler(this, &VrpnClient::handle_vel);
 
     std::cout << "Attempting connection to VRPN server at: " << full_address << std::endl;
 
@@ -122,15 +191,14 @@ VrpnClient::VrpnClient(std::string rigidBodyName, std::string server_address)
 
 VrpnClient::~VrpnClient()
 {
-
-	this->tracker->unregister_change_handler(this, &VrpnClient::handle_pose);
-	this->tracker->unregister_change_handler(this, &VrpnClient::handle_vel);
-	delete this->connection;
+   this->tracker->unregister_change_handler(this, &VrpnClient::handle_pose);
+   //this->tracker->unregister_change_handler(this, &VrpnClient::handle_vel);
+   delete this->connection;
 }
 
 void VrpnClient::mainloop()
 {
-	connection->mainloop();
-	tracker->mainloop();
-	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   connection->mainloop();
+   tracker->mainloop();
+   std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
